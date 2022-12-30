22654ed02607cf58c286ec82f58c00f35c3bd18f
Maksim Zinovev <iowa.dallas@mail.ru>
2022-12-30 22:36:47 +0300

    final changes

--- Navigation/CControllers/ProfileViewController.swift
+++ Navigation/CControllers/ProfileViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     private lazy var firstButton: UIButton = {
         let button = UIButton()
         var color: UIColor = .blue
@@ -9,24 +9,21 @@ class ProfileViewController: UIViewController {
         button.setTitle("Back", for: .normal)
         return button
     }()
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
-
         self.navigationItem.title = "Мой пост"
-
         self.navigationController?.navigationBar.prefersLargeTitles = true
-
-        view.addSubview(firstButton)
         firstButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-      
+        view.addSubview(firstButton)
+        
     }
-
+    
     @objc func buttonPressed() {
-           let profileViewController = ProfileViewController()
-
+        let profileViewController = ProfileViewController()
         present(profileViewController, animated: true)
     }
 }
+
